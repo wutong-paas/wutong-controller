de21954e839a16ac796dec7a628ba0374166d4ed
dp <me@srcio.cn>
2023-01-04 16:26:38 +0800

    update telepresence cluster role

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@
 
 export NAMESPACE=wutong-controller
 export VERSION=v1.0.0
-docker buildx create --use --name controllerbuilder
+# docker buildx create --use --name controllerbuilder
 # docker buildx use controllerbuilder
-docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
+# docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
 docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
 # docker buildx rm controllerbuilder
